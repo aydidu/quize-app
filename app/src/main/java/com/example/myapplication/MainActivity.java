c101359e83b05ce6b587c9e238d6704b3cde8786
Aysulu <aydidu0499@gmail.com>
2021-04-14 12:48:53 +0500

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,12 +12,17 @@ import com.google.android.material.tabs.TabLayout;
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private TextView textView;
+    private RadioButton rbA, rbB, rbC, rbD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tabLayout = findViewById(R.id.tabLayout);
         textView = findViewById(R.id.textView);
+        rbA = findViewById(R.id.radioButton);
+        rbB = findViewById(R.id.radioButton2);
+        rbC = findViewById(R.id.radioButton3);
+        rbD = findViewById(R.id.radioButton4);
         tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         PhysicsTest physicsTest = new PhysicsTest();
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                Toast.makeText(getApplicationContext(), "onTabSelected", Toast.LENGTH_LONG).show();
+
             }
 
             @Override
